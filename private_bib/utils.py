0a36b6d1f602deaafe38edfd44e22b2e9048e7c4
Julia Sprenger <julia.sprenger@rwth-aachen.de>
2017-10-17 15:09:13 +0200

    Make location recognition dependent on actual existence of session files

--- private_bib/utils.py
+++ private_bib/utils.py
@@ -11,20 +11,20 @@ import glob
 
 #class utilities():
 
-local = ''
-# if os.path.isdir('/home/julia/projects'):
+# local = ''
+# # if os.path.isdir('/home/julia/projects'):
+# #     local = 'laptop'
+# # el
+# if os.path.isdir('/mnt/Transcend/Datasets'):
+#     local = 'disc'
+# elif os.path.isdir('/home/julia/'):
 #     local = 'laptop'
-# el
-if os.path.isdir('/mnt/Transcend/Datasets'):
-    local = 'disc'
-elif os.path.isdir('/home/julia/'):
-    local = 'laptop'
-elif os.path.isdir('/users/sprenger/python/modules'):
-    local = 'hambach'
-elif os.path.isdir('/home/j.sprenger'):
-    local = 'blaustein'
-
-print('Working on data saved at {}'.format(local))
+# elif os.path.isdir('/users/sprenger/python/modules'):
+#     local = 'hambach'
+# elif os.path.isdir('/home/j.sprenger'):
+#     local = 'blaustein'
+#
+# print('Working on data saved at {}'.format(local))
 
 spp_struct = re.compile('.{4}-.{2}-.{2}_.{2}-.{2}-.{2}')
 
@@ -58,6 +58,8 @@ paths = {'laptop':{'hamburg':{'data':'/home/julia/data/SPP/data',
                       }
          }
 
+locs = list(paths)
+# print('Locations {}'.format(locs))
 
 # datapath = {'hamburg':{'laptop':'/home/julia/data/SPP1665/Data/devel-circuits/data',
 #                         'disc':'/mnt/Transcend/Datasets/SPP1665/Data/devel-circuits/data',
@@ -73,49 +75,71 @@ paths = {'laptop':{'hamburg':{'data':'/home/julia/data/SPP/data',
 monkey_translator = {'l': 'Lilou', 'n': 'Nikos', 'i': 'Nikos2', 't': 'Tanya',
                      's': 'Sana', 'a': 'Tanya2', 'e': 'Enya'}
 
-def get_data_location(filename=None):
-    group = 'hamburg' if spp_struct.match(filename) else 'marseille'
+def _find_loc(session, group, type=None):
+    loc = None
+    for id in range(len(locs)):
+        loc = locs[id]
+        if type is None:
+            path = paths[loc][group]
+        else:
+            path = paths[loc][group][type]
+        if (glob.glob(os.path.join(path, '{}*'.format(session)))
+                + glob.glob(os.path.join(path, '**', '{}*'.format(session)))):
+            break
+        loc = None
+
+    if loc is None:
+        raise ValueError('Can not determine location of {} {} for '
+                         'session {}.'.format(group, type, session))
+    return loc
+
+def get_data_location(session=None):
+    group = 'hamburg' if spp_struct.match(session) else 'marseille'
 
     # walking into monkey directory
-    refined_path = '/Data%s'%(monkey_translator[filename[0]]) if (group == 'marseille' and filename != None) else ''
+    refined_path = '/Data%s'%(monkey_translator[session[0]]) if (group == 'marseille' and session != None) else ''
 
-    return paths[local][group]['data'] + refined_path
+    loc = _find_loc(session=session, group=group, type='data')
+    return paths[loc][group]['data'] + refined_path
 
 
-def get_metadata_location(filename):
-    group = 'hamburg' if spp_struct.match(filename) else 'marseille'
+def get_metadata_location(session):
+    group = 'hamburg' if spp_struct.match(session) else 'marseille'
 
-    # walking into monkey meta data directory
-    refined_path = '/MetaData%s'%(monkey_translator[filename[0]]) if (group == 'marseille' and filename != None) else ''
-
-    return paths[local][group]['metadata'] + refined_path
-
-def get_cache_location(filename):
-    group = 'hamburg' if spp_struct.match(filename) else 'marseille'
+    loc = _find_loc(session=session, group=group, type='metadata')
 
     # walking into monkey meta data directory
-    refined_path = '/MetaData%s'%(monkey_translator[filename[0]]) if (group == 'marseille' and filename != None) else ''
+    refined_path = '/MetaData%s'%(monkey_translator[session[0]]) if (group == 'marseille' and session != None) else ''
 
-    if 'cache' not in paths[local][group]:
-        raise ValueError('No cache directory saved for %s %s'%(local,group))
-    return paths[local][group]['cache'] + refined_path
+    return paths[loc][group]['metadata'] + refined_path
+
+def get_cache_location(session):
+    group = 'hamburg' if spp_struct.match(session) else 'marseille'
+
+    # walking into monkey meta data directory
+    refined_path = '/MetaData%s'%(monkey_translator[session[0]]) if (group == 'marseille' and session != None) else ''
+
+    # using same location for caching as for data
+    loc = _find_loc(session=session, group=group, type='data')
+
+    if 'cache' not in paths[loc][group]:
+        raise ValueError('No cache directory saved for %s %s'%(loc, group))
+    return paths[loc][group]['cache'] + refined_path
 
 def get_result_location():
-    return paths[local]['result']
+    loc = _find_loc(group='result')
+    return paths[loc]['result']
 
-def get_cache_location(filename):
-    group = 'hamburg' if spp_struct.match(filename) else 'marseille'
-    return paths[local][group]['cache']
+def get_sort_location(session):
+    group = 'hamburg' if spp_struct.match(session) else 'marseille'
+    loc = _find_loc(group=group, type='sortdir')
+    return paths[loc][group]['sortdir']
 
-def get_sort_location(filename):
-    group = 'hamburg' if spp_struct.match(filename) else 'marseille'
-    return paths[local][group]['sortdir']
-
-def get_STA_predata_location(filename):
-    if local:
-        return "/home/julia/projects/Synchrofacts/synchrofact_manuscript/predata/%s"%(filename)
-    else:
-        return "/python/projects/synchrofacts/predata/%s"%(filename)
+# def get_STA_predata_location(session):
+#     if local:
+#         return "/home/julia/projects/Synchrofacts/synchrofact_manuscript/predata/%s"%(session)
+#     else:
+#         return "/python/projects/synchrofacts/predata/%s"%(session)
     
 
 def get_unsrt_unit255_sessions():
